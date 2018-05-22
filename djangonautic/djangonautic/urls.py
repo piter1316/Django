a794f21d9264bea78b8ee57ef668348f83f2c8c7
piter_1316 <piter_1316@wp.pl>
2018-05-22 15:58:02 +0200

    new app added for articles

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^articles/',include('articles.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.home_page),
+
+
 ]
